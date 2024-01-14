cd9f897ae2f68b4a2723625bbd805b274d907899
Jonas de Bruin <jonasdebruin05@gmail.com>
2024-01-14 13:40:53 +0100

    Fixes project files not generating

--- Setup-Windows.bat
+++ Setup-Windows.bat
@@ -1,47 +1,33 @@
 @echo off
 
 echo Choose an IDE to build:
-echo 1. vs2022
-echo 2. vs2019
-echo 3. vs2017
-echo 4. vs2015
-echo 5. vs2013
-echo 6. vs2012
-echo 7. vs2010
-echo 8. vs2008
-echo 9. vs2005
-echo 10. gmake
-echo 11. gmake2
-echo 12. xcode4
-echo 13. codelite
+echo vs2022
+echo vs2019
+echo vs2017
+echo vs2015
+echo vs2013
+echo vs2012
+echo vs2010
+echo vs2008
+echo vs2005
+echo gmake
+echo gmake2
+echo xcode4
+echo codelite
 
 set /p choice="Enter your choice (e.g., 1 for vs2022): "
 
-:: Define an array of IDE options
-set "ide[1]=vs2022"
-set "ide[2]=vs2019"
-set "ide[3]=vs2017"
-set "ide[4]=vs2015"
-set "ide[5]=vs2013"
-set "ide[6]=vs2012"
-set "ide[7]=vs2010"
-set "ide[8]=vs2008"
-set "ide[9]=vs2005"
-set "ide[10]=gmake"
-set "ide[11]=gmake2"
-set "ide[12]=xcode4"
-set "ide[13]=codelite"
 
 :: Get the selected IDE from the array
-if "%choice%" gtr "0" if "%choice%" lss "14" (
-    Scripts\Binaries\Premake\Windows\premake5.exe --file=Build.lua !ide[%choice%]!
-) else (
-    echo Invalid choice! Exiting...
-    exit /b
-)
+call premake5 --file=Build.lua %choice%
 
 echo Premake setup completed!
 
-%LOCALAPPDATA%\Microsoft\WindowsApps\python3.12.exe Scripts/setup.py
+:: Pause the script to see the output before closing
+
+
+python3 Scripts/setup.py
 
 echo Setup completed!
+
+pause
