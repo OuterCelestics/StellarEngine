dc3fad5084448eb037d8edb25b15c546333abf1b
Jonas de Bruin <jonasdebruin05@gmail.com>
2024-04-21 23:35:47 +0200

    Basic camera funcitons

--- Engine/source/engine/components/camera/camera.h
+++ Engine/source/engine/components/camera/camera.h
@@ -7,4 +7,9 @@ public:
 	glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
 	glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 	glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
+
+public:
+	void SetCameraPosition(float x, float y, float z);
+	void SetCameraFront(float x, float y, float z);
+	void SetCameraUp(float x, float y, float z);
 };
