0e05eb288f22c9bc3fc9c4d79015da56789caf13
Luke <luckyskywalker2003@gmail.com>
2024-04-22 13:24:56 +0200

    Formatted the client app & added service locator

---
+++ Application/source/main.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "engine.h"
+
+namespace fs = std::filesystem;
+using namespace std;
+
+namespace Editor {
+	class Source : public Engine::Application
+	{
+	public:
+		Source();
+		~Source();
+	private:
+	
+	};
+}
