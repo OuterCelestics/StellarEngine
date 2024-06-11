69ca755e0eebf6cf2b415778c0a6f758d6f80547
Luke <luckyskywalker2003@gmail.com>
2024-06-11 16:19:28 +0200

    Prepared the renderer class for the whole new renderer we need to write in order to get ECS working

--- StellarEngine/source/engine/rendering/opengl_pipeline.cpp
+++ StellarEngine/source/engine/rendering/opengl_pipeline.cpp
@@ -1,157 +1,39 @@
 #include "opengl_pipeline.h"
-#include <glad/glad.h>
-#include "glm.hpp" 
-#include <gtc/matrix_transform.hpp>
-#include <gtc/type_ptr.hpp>
 #include <iostream>
+#include "../components/error/debug.h"
 #include "../components/time/time.h"
 
 namespace Engine
 {
-	float vertices[] = {
-		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-		 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-	};
-
 	void Pipeline::Initialize()
 	{
-		// Load gladGLAD
-		if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+		if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 		{
-			std::cout << "Failed to initialize GLAD" << std::endl;
+			std::cout << "OpenGL Version: " << GLVersion.major << "." << GLVersion.minor << std::endl;
 		}
-		
-		// Enable depth test so we have proper ordening
+		else
+		{
+			Debug::ErrorLog("Failed to initialize GLAD", true);
+		}
+
+		// Enable depth testing & multisampling
 		glEnable(GL_DEPTH_TEST);
+		glEnable(GL_MULTISAMPLE);
 
-		// Section
-		shaderProgram = new Shaders;
-		shaderProgram->Compile("shaders/default.vs", "shaders/default.frag");
-
-		size_t size = sizeof(vertices) / sizeof(vertices[0]);
-
-		VAO1 = new VAO();
-		VBO1 = new VBO();
-		VAO1->Bind();	
-		VAO1->LinkVBO(VBO1, vertices, size);
-		VAO1->Unbind();
-		VBO1->Unbind();
-		textureID.TextureInit();
+		// TODO: Implement shader class and buffer class as static classes
 	}
-
-	glm::vec3 cubePositions[] = {
-		glm::vec3( 0.0f,  0.0f,  0.0f),
-		glm::vec3( 2.0f,  5.0f, -15.0f),
-		glm::vec3(-1.5f, -2.2f, -2.5f),
-		glm::vec3(-3.8f, -2.0f, -12.3f),
-		glm::vec3( 2.4f, -0.4f, -3.5f),
-		glm::vec3(-1.7f,  3.0f, -7.5f),
-		glm::vec3( 1.3f, -2.0f, -2.5f),
-		glm::vec3( 1.5f,  2.0f, -2.5f),
-		glm::vec3( 1.5f,  0.2f, -1.5f),
-		glm::vec3(-1.3f,  1.0f, -1.5f)
-	};
 
 	void Pipeline::Render(ConfigLoader* config, float* aspect_ratio, Camera* camera)
 	{
-		// Update all the timing stuff
 		Time::UpdateTime();
-	
-		// Print fps
-		float fps = Time::GetFramesPerSecond(1.5f);
-		if (fps != -1) std::cout << "FPS " << fps << std::endl;
 
 		// Clear the screen
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		
-		// Create transformations
-		// Make sure to initialize matrix to identity matrix first
-		glm::mat4 view = glm::mat4(1.0f);
-		float radius = 10.0f;
-		glm::mat4 projection = glm::mat4(1.0f);
-
-		// Simple projection
-		projection = glm::perspective(glm::radians(45.0f), *aspect_ratio, 0.1f, 100.0f);
-
-		//view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-
-		// Walk around cam using camera
-		view = glm::lookAt(camera->cameraPos, camera->cameraPos + camera->cameraFront, camera->cameraUp);
-
-		shaderProgram->Activate();
-		shaderProgram->setMat4("projection", projection);
-		shaderProgram->setMat4("view", view);
-		textureID.Bind();
-		VAO1->Bind();
-
-		glActiveTexture(GL_TEXTURE0);
-		textureID.Bind();
-
-		for (unsigned int i = 0; i < 10; i++)
-		{
-			// calculate the model matrix for each object and pass it to shader before drawing
-			glm::mat4 model = glm::mat4(1.0f);
-			model = glm::translate(model, cubePositions[i]);
-			float angle = 20.0f * i;
-
-			if (i % 3 == 0)
-				angle = glfwGetTime() * 25.0f;
-
-			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-			shaderProgram->setMat4("model", model);
-
-			glDrawArrays(GL_TRIANGLES, 0, 36);
-		}
-
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-
-		glActiveTexture(GL_TEXTURE0);
-
-		glUseProgram(0);
 	}
 
 	void Pipeline::Terminate()
 	{
-		
+
 	}
 }
